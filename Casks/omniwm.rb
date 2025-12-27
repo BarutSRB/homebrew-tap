8c9e1dbec04b33f3b0e93caad0619d4389563153
Barut <barut@barutsmacstudio.home>
2025-12-26 20:21:44 -0500

    Update OmniWM to v0.1.3

--- Casks/omniwm.rb
+++ Casks/omniwm.rb
@@ -1,6 +1,6 @@
 cask "omniwm" do
-  version "0.1.2"
-  sha256 "cce91f25517fbf33ea1dfb3dbe77f31093304b1f9b4c88e451006aaeaa29fcb7"
+  version "0.1.3"
+  sha256 "c29c06cca5f111a11b3696dbb56def77676a61327733655858f074a0305ee3fc"
 
   url "https://github.com/BarutSRB/OmniWM/releases/download/v#{version}/OmniWM-v#{version}.zip"
   name "OmniWM"
