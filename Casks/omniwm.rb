472fd36fe6f47803b2d3190e5d3cfd7230b13451
Barut <barut@barutsmacstudio.home>
2026-01-22 20:47:31 -0500

    Update omniwm to 0.2.7.2

--- Casks/omniwm.rb
+++ Casks/omniwm.rb
@@ -1,6 +1,6 @@
 cask "omniwm" do
-  version "0.2.7.1"
-  sha256 "35827362e1e065e7388b3f0689ea9ebe84f5b118c62b1ad376aacb462b6128da"
+  version "0.2.7.2"
+  sha256 "70ff8cc4c1317439014e5b885b08b661b93f093c2a3812183156095646c66611"
 
   url "https://github.com/BarutSRB/OmniWM/releases/download/v#{version}/OmniWM-v#{version}.zip"
   name "OmniWM"
