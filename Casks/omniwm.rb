30784e862fa4c183f2ad067217e78349be6db6b6
Barut <barut@barutsmacstudio.home>
2026-04-21 18:37:19 -0400

    release: omniwm 0.4.8

--- Casks/omniwm.rb
+++ Casks/omniwm.rb
@@ -1,6 +1,6 @@
 cask "omniwm" do
-  version "0.4.7.4"
-  sha256 "e8f38cb479f9475090733dbd40e46b25a12e07130f7bb6984ed9f4556a505c8a"
+  version "0.4.8"
+  sha256 "26b4cacc344dc506c0e1cd8a109af56da83e4037e0ccdb0d53f90db1ef01fe16"
 
   url "https://github.com/BarutSRB/OmniWM/releases/download/v#{version}/OmniWM-v#{version}.zip"
   name "OmniWM"
